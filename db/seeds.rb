7021c5c8e29421c020bcb58c32ed2a67a0d1344d
Sophie Rost <laram.risgen@gmail.com>
2018-11-13 15:56:59 +0100

    Add controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
+
+10.times do
+  my_title = Faker::HarryPotter.quote
+  my_content = Faker::MostInterestingManInTheWorld.quote
+  Article.new(title: my_title, content: my_content)
+end
